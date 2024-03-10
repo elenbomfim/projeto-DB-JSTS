717577241e3376cff39cb8b9fd5eda33a4670faf
KEO <keoxcoelho@gmail.com~>
2024-03-10 15:31:09 -0300

    Trabalhando nos métodos de Pessoa

--- keo-scripts/BancoDeDados.js
+++ keo-scripts/BancoDeDados.js
@@ -6,10 +6,46 @@ class BancoDeDados extends Pessoa {
         super(nome, idade, email)
     } 
 
+    adicionarPessoanoDB() {
+        // herdar metodo inputNome, inputIdade, inputEmail
+        this.inputNome(),
+        this.inputIdade(),
+        this.inputEmail()
+
+      console.log(`${this._nome},  ${this._idade} anos, email ${this._email}adicionado(a) no banco de dados`)
+    }
+
+    listarPessoasNoDB() {
+        // herda método listarPessoasParaDB
+        console.log(`Listando pessoas do banco de dados: ${this.listarPessoasNoDB()}`)
+    }
+
+    buscarPessoaNoDB() {
+        // herda método buscarPessoaParaDB
+        console.log(`Buscando pessoa no banco de dados: ${this.buscarPessoaNoDB()}`)
+
+    }
+
+    atualizarPessoaNoDB() {
+        // herda método atualizarPessoaParaDB
+        console.log(`Atualizando pessoa no banco de dados: ${this.atualizarPessoaNoDB()}`)
+    }
+
+    removerPessoaNoDB() {
+        // herda método removerPessoaParaDB
+        console.log(`Removendo pessoa do banco de dados: ${this.removerPessoaNoDB()}`)
+    }
 
 }
 
-const novaPessoaNoDB = new BancoDeDados('Arthur', 29, 'a@a.com')
-console.log(novaPessoaNoDBpessoaDB)
-console.log(novaPessoaNoDBpessoaDB.exibirInfos()) // herda métodos
+// const novaPessoaNoDB = new BancoDeDados('Arthur', 29, 'a@a.com')
+// console.log(novaPessoaNoDBpessoaDB)
+// console.log(novaPessoaNoDBpessoaDB.exibirInfos()) // herda métodos
+
+const pessoaNoDB = new BancoDeDados
+pessoaNoDB.adicionarPessoanoDB()
+pessoaNoDB.listarPessoasNoDB()
+pessoaNoDB.buscarPessoaNoDB()
+pessoaNoDB.atualizarPessoaNoDB()
+pessoaNoDB.removerPessoaNoDB()
 
--- keo-scripts/Pessoa.js
+++ keo-scripts/Pessoa.js
@@ -1,3 +1,5 @@
+import { list } from "@chakra-ui/react"
+
 export default class Pessoa  { // abstração
     constructor(nome = String, idade = Number, email = String) { // validação
         this._nome = nome
@@ -5,15 +7,52 @@ export default class Pessoa  { // abstração
         this._email = email
     }
 
-    exibirInfos() {
-        return `${this._nome}, ${this._idade}, ${this._email}`
+    adicionarPessoanoDB() {
+        const pessoa = new Pessoa(this._nome, this._idade, this._email)
+        listaDePessoas.push(this._nome, this._idade, this._email)
+
+        return `Adicionando pessoa no banco de dados: ${this.pessoa}`
+    }
+
+    listarPessoasParaDB() {
+        listaDePessoas = this.pessoa.push()
+        listaDePessoas = listaDePessoas.lenght > 0 ? listaDePessoas : 'Não há pessoas cadastradas'
+        const lista = [listaDePessoas]
+
+        return `Listando pessoas do banco de dados: ' + ${listaDePessoas}`
+
+    }
+
+    buscarPessoaParaDB() {
+        cost buscarPessoa = this.listarPessoasParaDB.find(pessoa => pessoa === this._nome, this._idade, this._email)
+
+        return `Buscando pessoa no banco de dados: ${this._nome}, ${this._idade}, ${this._email}`
+    }
+
+    atualizarPessoaParaDB() {
+        this.listarPessoasParaDB.map(pessoa => pessoa === this._nome, this._idade, this._email)
+
+        return `Atualizando pessoa no banco de dados: ${this._nome}, ${this._idade}, ${this._email}`
+    }
+
+    removerPessoaParaDB() {
+        this.listarPessoasParaDB.filter(pessoa => pessoa !== this._nome, this._idade, this._email)
+
+        return `Removendo pessoa do banco de dados: ${this._nome}, ${this._idade}, ${this._email}`
     }
 }
 
-// const novaPessoaInserida = new Pessoa('Keo', 37, 'l@l.com') // instanciando
-// console.log(novaPessoaInserida)
-// console.log(novaPessoaInserida.exibirInfos())
+const pessoa = new Pessoa ("Jovem Tranquilão", "100", "jt@jt.com")
+console.log(pessoa) // Pessoa { _nome: 'Jovem Tranquilão', _idade: '100', _email: 'jt@jt.com' }
 
-// criando o package.json
-// npm init -y
-// "type": "module", // no package.json
+const pessoa2 = new Pessoa ("Elen", 20, "e@e.com")
+console.log(pessoa2) // Pessoa { _nome: 'Elen', _idade: 20, _email: '
+
+const listaDePessoas = [Pessoa.length]
+
+const busca 
+console.log(buscarPessoaParaDB(pessoa2))
+
+
+
+
