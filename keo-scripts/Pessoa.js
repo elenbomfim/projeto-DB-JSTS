2c95e848b7150d72ebd450d89ea485c1562a01dd
KEO <keoxcoelho@gmail.com~>
2024-03-10 13:10:34 -0300

    Criada a classe Pessoa com configurações básicas de abstração

---
+++ keo-scripts/Pessoa.js
@@ -0,0 +1,15 @@
+class Pessoa  { // abstração
+    constructor(nome = String, idade = Number, email = String) { // validação
+        this._nome = nome
+        this._idade = idade 
+        this._email = email
+    }
+
+    exibirInfos() {
+        return `${this._nome}, ${this._idade}, ${this._email}`
+    }
+}
+
+const novaPessoaInserida = new Pessoa('Keo', 37, 'l@l.com') // instanciando
+console.log(novaPessoaInserida)
+console.log(novaPessoaInserida.exibirInfos())
