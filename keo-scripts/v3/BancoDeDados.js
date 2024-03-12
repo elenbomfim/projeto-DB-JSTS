65d341f042e30c8ee8a890afbf327d36e0ccd8d6
KEO <keoxcoelho@gmail.com~>
2024-03-12 19:33:11 -0300

    Finalizado método listar

---
+++ keo-scripts/v3/BancoDeDados.js
@@ -0,0 +1,52 @@
+import Pessoa from './Pessoa.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs'; 
+
+class BancoDeDados extends Pessoa {
+    constructor(_nome, _idade, _email) {
+        super(_nome, _idade, _email);
+    }
+
+    async listarPessoasNoDB() {
+        try {
+            const currentFileUrl = import.meta.url;
+            const currentFilePath = fileURLToPath(currentFileUrl);
+            const filePath = path.resolve(path.dirname(currentFilePath), 'db.json');
+            const data = fs.readFileSync(filePath, 'utf-8'); // le arquivo usando fs.readFileSync
+            const json = JSON.parse(data); // parse do conteudo para JSON
+            const listaDePessoas = json.listaDePessoas;
+            console.log("Lista de pessoas do DB:", listaDePessoas);
+        } catch (error) {
+            console.error('Erro ao carregar o JSON:', error);
+        }
+    }
+}
+
+// buscarPessoaNoDB() {
+
+// }
+
+// atualizarPessoaNoDB() {
+
+// }
+
+// removerPessoaNoDB() {
+
+// }
+
+// const novaPessoaNoDB = new BancoDeDados('Arthur', 29, 'a@a.com')
+// console.log(novaPessoaNoDBpessoaDB)
+// console.log(novaPessoaNoDBpessoaDB.exibirInfos()) // herda métodos
+
+const bancoDeDados = new BancoDeDados();
+bancoDeDados.listarPessoasNoDB();
+
+
+
+
+
+
+
+
+
---
+++ keo-scripts/v3/Pessoa.js
@@ -0,0 +1,50 @@
+export default class Pessoa { // abstração
+    constructor(_nome = String, _idade = Number, _email = String) { // validação
+        this.__nome = _nome
+        this.__idade = _idade
+        this.__email = _email
+    }
+
+    criarNovaPessoa() {
+        const pessoa = new Pessoa(this.__nome, this.__idade, this.__email)
+        const listaDePessoas = []
+        const enviarPessoaParaLista = listaDePessoas.push(pessoa)
+        return enviarPessoaParaLista
+    }
+}
+
+// encapsula logica de envio de dados
+class PessoaService {
+    constructor(url) {
+        this.url = "http://localhost:3000/listaDePessoas";
+    }
+
+    enviarPessoa(_nome, _idade, _email) {
+        const pessoa = new Pessoa(_nome, _idade, _email);
+        const body = JSON.stringify(pessoa);
+        return fetch(this.url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body,
+        });
+    }
+}
+
+const pessoaService = new PessoaService("http://localhost:3000/listaDePessoas")
+
+const _nome = "Bebeto"
+const _idade = 40
+const _email = "bebeto@brasil.com"
+
+try {
+    const response = await pessoaService.enviarPessoa(_nome, _idade, _email)
+    if (!response.ok) {
+        throw new Error("Erro ao registrar. Tente novamente.")
+    }
+    const data = await response.json()
+    console.log("Registro efetivado com sucesso", data)
+} catch (error) {
+    console.error("Ops... " + error.message)
+}
